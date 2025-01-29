25553f671ef562036f4f072a5eace6e475bbef8e
Steve Murigi <steve.murigi@student.moringaschool.com>
2025-01-29 11:42:21 -0600

    variable

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_restful import Api, Resource
 from models import db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
